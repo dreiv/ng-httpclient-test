f9b17c058653a496a07b6a093467fb6a025493a4
dreiv <dreivoicu@gmail.com>
2017-11-14 19:43:12 +0200

    wrote some http service code to test.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocationService} from './location.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  locations: Observable<any>;
+
+  constructor(private location: LocationService) {
+  }
+
+  ngOnInit(): void {
+    this.locations = this.location.getLocations();
+  }
 }
